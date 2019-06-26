98ff2fc84f79dda5d9d435ae2e98ef5ff9fdf5a2
natriy1 <natriy1@gmail.com>
2019-06-26 12:47:02 +0300

    Update and rename titanic_6HW.py to titanic_6.py

--- titanic_6.py
+++ titanic_6.py
@@ -4,14 +4,12 @@ df = pd.read_csv('train.csv')
 sex = list(df['Sex'])
 ln = len(sex)
 
-# Замещаем male и female на 1 и 0 соответственно
 for i in range(ln):
     if sex[i] == 'male':
         sex[i] = 1
     else:
         sex[i] = 0
 
-# Преобразуем в DataFrame
 sx = pd.DataFrame({'Sex':sex})
 
 print(df['Age'].corr(df['Survived']))
